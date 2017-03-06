637b365dd87d8d41ab16547344ae5f9d8d2d74d3
AIZAWA Hina <hina@bouhime.com>
2017-03-07 05:40:59 +0900

    add missing file

---
+++ webapp/config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'class' => 'yii\\db\\Connection',
+    'dsn' => 'pgsql:host=pgsql;port=5432;dbname=statink2testfire',
+    'username' => 'statink2',
+    'password' => 'password',
+    'charset' => 'UTF-8',
+    'enableSchemaCache' => true,
+    'schemaCache' => 'schemaCache',
+];
+
