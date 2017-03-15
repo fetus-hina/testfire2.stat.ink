8302a79ad583e6e10e3db2c518ebcc4802553d32
AIZAWA Hina <hina@bouhime.com>
2017-03-15 22:26:17 +0900

    画像が保存されない問題の修正・assetsが更新されないもんだいの修正
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

--- webapp/docker/entrypoint.sh
+++ webapp/docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 cd $(dirname $0)/..
-chown -R webapp:webapp /var/www/site/runtime /var/www/site/web/assets
+chown -R webapp:webapp \
+    /var/www/site/runtime \
+    /var/www/site/web/assets \
+    /var/www/site/web/images \
+    /var/www/site/web/profile-images
+rm -rf /var/www/site/web/assets/*
 su -c 'scl enable php71 -- ./yii migrate/up --interactive=0' webapp
 /opt/remi/php71/root/usr/sbin/php-fpm --nodaemonize
